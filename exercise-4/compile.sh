b2ffcc2afaa2e87099564e41c393edb3c4ca49a9
Albert Rise Nielsen <albert@risenielsen.dk>
2023-01-09 16:51:44 +0100

    4.2 prep

--- exercise-4/compile.sh
+++ exercise-4/compile.sh
@@ -4,4 +4,4 @@ binDir=/data/bin
 $binDir/compileParser.sh Fun
 $binDir/compileLexer.sh Fun
 
-$binDir/run.sh Absyn.fs Fun.fs FunPar.fs FunLex.fs Parse.fs ParseAndRun.fs
+$binDir/run.sh Absyn.fs Fun.fs FunPar.fs FunLex.fs Parse.fs ParseAndRun.fs HigherFun.fs ParseAndRunHigher.fs
