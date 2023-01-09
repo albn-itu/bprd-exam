57412271a7685410b678139b3b66fa691fa086c7
Albert Rise Nielsen <albert@risenielsen.dk>
2023-01-09 14:33:05 +0100

    2

--- exercise-2/compile.sh
+++ exercise-2/compile.sh
@@ -5,3 +5,6 @@ $binDir/compileParser.sh C
 $binDir/compileLexer.sh C
 
 fsharpc --standalone -r $binDir/FsLexYacc.Runtime.dll Absyn.fs CPar.fs CLex.fs Parse.fs Machine.fs Comp.fs ListCC.fs -o listcc.exe
+
+# cd ListVM/ListVM
+# gcc -o listmachine listmachine.c
