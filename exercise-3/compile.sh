04b6e993375de26ace53d15d9d184655e9791377
Albert Rise Nielsen <albert@risenielsen.dk>
2023-01-09 19:39:52 +0100

    Done

--- exercise-3/compile.sh
+++ exercise-3/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 binDir=/data/bin
 
-$binDir/compileParser.sh C
 $binDir/compileLexer.sh C
+$binDir/compileParser.sh C
 
 $binDir/run.sh Absyn.fs CPar.fs CLex.fs Parse.fs Interp.fs ParseAndRun.fs Machine.fs Comp.fs ParseAndComp.fs
